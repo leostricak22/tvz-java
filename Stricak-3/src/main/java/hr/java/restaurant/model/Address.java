6557206fd8bd43a01e91381f71fd7f51e22f7d91
leostricak22 <stricak.leo@gmail.com>
2024-11-14 17:05:36 +0100

    feat: JavaDoc test

--- Stricak-3/src/main/java/hr/java/restaurant/model/Address.java
+++ Stricak-3/src/main/java/hr/java/restaurant/model/Address.java
@@ -2,10 +2,17 @@ package hr.java.restaurant.model;
 
 import hr.java.service.Input;
 import hr.java.service.Output;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
 
+/**
+ * Represents an address of a restaurant.
+ * */
 public class Address extends Entity {
+    private static final Logger logger = LoggerFactory.getLogger(Address.class);
+
     private static Long counter = 0L;
 
     private String street;
@@ -13,6 +20,11 @@ public class Address extends Entity {
     private String city;
     private String postalCode;
 
+    /**
+     * Constructs an Address object using the provided builder.
+     *
+     * @param builder the builder instance containing address information
+     */
     private Address(Builder builder) {
         super(builder.id);
         this.street = builder.street;
@@ -53,7 +65,14 @@ public class Address extends Entity {
         this.postalCode = postalCode;
     }
 
+    /**
+     * Prompts the user to input address details using a scanner.
+     *
+     * @param scanner the Scanner object to read user input
+     * @return a new Address instance with the entered details
+     */
     public static Address inputAddress(Scanner scanner) {
+        logger.info("Address input.");
         String addressStreet = Input.string(scanner, "Unesite ulicu restorana: ");
         String addressHouseNumber = Input.string(scanner, "Unesite kućni broj restorana: ");
         String addressCity = Input.string(scanner, "Unesite grad u kojem se nalazi restoran: ");
@@ -69,11 +88,20 @@ public class Address extends Entity {
                 .build();
     }
 
+    /**
+     * Prints the address details with the specified number of tabulators.
+     *
+     * @param tabulators the number of tabulators to format the output
+     */
     public void print(Integer tabulators) {
+        logger.info("Printing address.");
         Output.tabulatorPrint(tabulators);
         System.out.println("Id: " + this.getId() + ", Ulica: " + this.street + ", Kućni broj: " + this.houseNumber + ", Grad: " + this.city + ", Poštanski broj: " + this.postalCode);
     }
 
+    /**
+     * Builder class for creating instances of {@link Address}.
+     */
     public static class Builder {
         private Long id;
         private String street;
